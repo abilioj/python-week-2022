8bcf258674a9861b28b76c56632ea5659ad8a69f
abilioj <abilioj@live.com>
2022-09-08 17:10:10 -0300

    up - model e database

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,8 +1,12 @@
 # from dataclasses import dataclass
 
+from email.policy import default
 from typing import Optional
 from sqlmodel import SQLModel, Field
 from sqlmodel import select
+from pydantic import validator
+from statistics import mean #pra estatitica
+from datetime import datetime
 
 # @dataclass
 # class Beer:
@@ -14,6 +18,20 @@ class Beer(SQLModel, table=True):
     flavor: int
     image: int
     cost: int
+    rate: int = 0
+    data: datetime = Field(default_factory=datetime.now)
 
+    # valida campo de 1 a 10
+    @validator("flavor","image","cost")
+    def validate_ratings(cls, v, field):
+        if v < 1 or v > 10:
+            raise RuntimeError(f"{field.name} must be between 1 and 10")
+        return v
 
-brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=8, cost=8)
+    # valida a pontuação da bebida,
+    @validator("rate",always=True)
+    def calculete_rate(cls, v, values):
+        rate = mean([values["flavor"], values["image"], values["cost"]])
+        return int(rate)
+
+brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=5, cost=8)
