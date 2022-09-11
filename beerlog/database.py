ff4912f0a27fc2b0d20b5c1fc0954d0f001b8993
Abílio José <ajgfqbu@gmail.com>
2022-09-11 05:31:40 +0000

    correções Warn database

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,10 +1,21 @@
-from sqlmodel import create_engine
+import warnings
+from sqlalchemy.exc import SAWarning
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
+
+from sqlmodel import create_engine, Session
+
+from beerlog import models
 from beerlog.config import settings
 
-# impotar o model beer
-from beerlog import models
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
 
-engine = create_engine(settings.database.url)
 
-# gerar a table no banco da model Beer
+engine = create_engine(settings.database.url, echo=False)
 models.SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    return Session(engine)
